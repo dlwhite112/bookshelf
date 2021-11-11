4fdfb62af2c7a5ea6d4c15dbc48569fe1d3b34da
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-11-10 18:45:44 -0600

    heroku app

---
+++ client/build/precache-manifest.72640f255fce071f42b1d2eb875c3b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f551d856eb3f52b75b16bde52c41fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d78eac79c7ea44c31cf",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "6c24a3950aef308b980b",
+    "url": "/static/js/2.5e4c0358.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.5e4c0358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d78eac79c7ea44c31cf",
+    "url": "/static/js/main.e2bb6b08.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
